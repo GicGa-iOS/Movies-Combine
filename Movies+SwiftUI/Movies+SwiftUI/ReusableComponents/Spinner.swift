d8c6dcaa968fc533e7944c2b4eb07ec732584db3
Giancarlo <castag.gian@gmail.com>
2022-03-22 12:50:53 -0500

    fix(popular-movies): add pagination to fetch popular movies service

---
+++ Movies+SwiftUI/Movies+SwiftUI/ReusableComponents/Spinner.swift
@@ -0,0 +1,23 @@
+//
+//  Spinner.swift
+//  Movies+SwiftUI
+//
+//  Created by Giancarlo Castañeda Garcia on 22/03/22.
+//
+
+import SwiftUI
+import UIKit
+
+struct Spinner: UIViewRepresentable {
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: style)
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        return spinner
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
