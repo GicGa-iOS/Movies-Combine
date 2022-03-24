5eb5024f87a492bde0b21ba25ec6835c0aac8ff3
Giancarlo <castag.gian@gmail.com>
2022-03-24 17:22:14 -0500

    feat(searcher): add custom UI

--- Movies+SwiftUI/Movies+SwiftUI/Features/Searcher/Views/SearcherScreen.swift
+++ Movies+SwiftUI/Movies+SwiftUI/Features/Searcher/Views/SearcherScreen.swift
@@ -1,8 +1,44 @@
 import SwiftUI
 
 struct SearcherScreen: View {
+
+    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    var rows: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+
+    @State private var searchText = ""
+
     var body: some View {
-        Text("Searcher")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: rows) {
+                    ForEach(searchResults, id: \.self) { name in
+                        NavigationLink(destination: Text(name)) {
+                            Text(name)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Search")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color.black)
+        }.searchable(text: $searchText,
+                     placement: .navigationBarDrawer(displayMode: .always),
+                     prompt: "Search"
+        ) {
+            ForEach(searchResults, id: \.self) { result in
+                Text("Are you looking for \(result)?")
+                    .searchCompletion(result)
+            }
+        }
+    }
+
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return names
+        } else {
+            return names.filter { $0.contains(searchText) }
+        }
     }
 }
 
